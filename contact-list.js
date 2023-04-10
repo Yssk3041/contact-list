c2e3a419d71e92291769f5c3f4c2c1f6a10e2ad8
Yessica Sotomayor C <ysotocastro@gmail.com>
2023-04-09 23:40:56 -0500

    Modificando el objeto, funcion adicionar contacto, mostrar contactos y eliminar contacto

--- contact-list.js
+++ contact-list.js
@@ -3,50 +3,75 @@ let contactos = [];
 contactos = [
   {
     id: 1,
-    nombre: "Yennifer Marquez",
+    nombre: "Yennifer",
+    apellidos: "Marquez",
     telefono: 3124322112,
+    ubicaciones: {
+      ciudad: "Apartadó",
+      direccion: "Calle 12 No. 22 - 45",
+    },
   },
   {
     id: 2,
-    nombre: "Jose Sossa",
+    nombre: "Jose ",
+    apellidos: "Sossa",
     telefono: 3011112121,
+    ubicaciones: {
+      ciudad: "Santander",
+      direccion: "Cra 43 No. 21 - 10",
+    },
   },
   {
     id: 3,
-    nombre: "Diana Camargo",
+    nombre: "Diana",
+    apellidos: "Camargo",
     telefono: 3220101010,
+    ubicaciones: {
+      ciudad: "Seattle",
+      direccion: "Avenue 5th 23 - 12",
+    },
   },
   {
     id: 4,
-    nombre: "Juan Pablo Caro",
+    nombre: "Juan Pablo",
+    apellidos: "Caro",
     telefono: 3040101001,
+    ubicaciones: {
+      ciudad: "Manizales",
+      direccion: "Avenida 4 No. 12 - 32",
+    },
   },
 ];
 
 const mostrarContactos = function (contacto) {
   for (let contacts of contacto.values()) {
     console.log(
-      `Contacto: ${contacts.nombre} \n Teléfono: ${contacts.telefono}\n --------------`
+      `Contacto: ${contacts.nombre} ${contacts.apellidos}\nTeléfono: ${contacts.telefono}\nDirección: ${contacts.ubicaciones.direccion} \nCiudad:${contacts.ubicaciones.ciudad}\n --------------`
     );
   }
 };
 
-function adicionarContacto(nombre_completo, telefono) {
+function adicionarContacto(nombre, apellido, telefono, ciudad, direccion) {
   console.log("Adicionando Contacto\n****");
   let id_contacto = contactos[contactos.length - 1].id;
 
   contactos.push({
     id: id_contacto + 1,
-    nombre: nombre_completo,
+    nombre: nombre,
+    apellidos: apellido,
     telefono: telefono,
+    ubicaciones: {
+      ciudad: ciudad,
+      direccion: direccion,
+    },
   });
 
   mostrarContactos(contactos);
 }
 
-const eliminarContacto = (nombre) => {
+const eliminarContacto = (nombre, apellido) => {
   for (let contacts of contactos) {
-    if (contacts.nombre == nombre) {
+    if (contacts.nombre == nombre && contacts.apellidos == apellido) {
       contactos.splice(contacts.id - 1, 1);
     }
   }
@@ -54,8 +79,21 @@ const eliminarContacto = (nombre) => {
   mostrarContactos(contactos);
 };
 
-adicionarContacto("Juan Florez", 3019080921);
+adicionarContacto(
+  "Juan",
+  "Florez",
+  3019080921,
+  "Manizales",
+  "Avenida 8 oeste No. 32 - 45"
+);
 console.log("");
-adicionarContacto("Jhon Paniagua", 3231298102);
+adicionarContacto(
+  "Jhon",
+  "Paniagua",
+  3231298102,
+  "Medellín",
+  "Cra 18 No. 87 - 98"
+);
 console.log("");
-eliminarContacto("Jose Sossa");
+
+eliminarContacto("Juan Pablo", "Caro");
